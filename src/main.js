be0b7762ccf3c0eef5d4c9a7ab7f5c2eeddd6afc
Andre Neves <andre@zebedee.io>
2022-05-21 20:47:00 -0400

    chore: base

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const Container = styled.div``;
+const Logo = styled.p``;
+const Description = styled.p``;
+
+function App() {
+  return (
+    <Container>
+      <Logo>nbd</Logo>
+      <Description>open source bitcoin & lightning development</Description>
+    </Container>
+  );
+}
+
+export default App;
